dc3432f75f61a307253c91f8f9a8ce4a5cbeefd1
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-08-02 07:20:41 +0600

    CORS Configuration

--- HotelListing/HotelListing/Program.cs
+++ HotelListing/HotelListing/Program.cs
@@ -2,15 +2,8 @@
 using Serilog;
 
 
-
-    var builder = WebApplication.CreateBuilder(args);
-
-     builder.Host.UseSerilog((ctx, lc) => lc
-    .WriteTo.Console()
-    .ReadFrom.Configuration(ctx.Configuration));
-
-    
-
+var AllowAll = "_myAllowAllOrigins";
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var logger = new LoggerConfiguration()
@@ -27,7 +20,16 @@ builder.Services.AddControllers();
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
 
-    var app = builder.Build();
+builder.Services.AddCors(options=> {
+    options.AddPolicy(name: AllowAll, policy => {
+        policy.AllowAnyOrigin()
+     .AllowAnyMethod()
+     .AllowAnyHeader();
+    
+    });
+});
+
+    var app = builder.Build();    
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -36,12 +38,12 @@ if (app.Environment.IsDevelopment())
         app.UseSwaggerUI();
     }
 
+
+
     app.UseHttpsRedirection();
-
+    app.UseCors(AllowAll);
     app.UseAuthorization();
-
     app.MapControllers();
-
     app.Run();
 
 
