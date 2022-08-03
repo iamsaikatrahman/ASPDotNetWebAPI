c45d3947e8dd78f1b3e17c32fd616f8ab2847bc8
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-08-03 23:07:23 +0600

    Setup AutoMapper and DTOs

--- HotelListing/HotelListing/Program.cs
+++ HotelListing/HotelListing/Program.cs
@@ -1,4 +1,5 @@
 
+using HotelListing.Configurations;
 using HotelListing.Data;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
@@ -24,6 +25,9 @@ builder.Services.AddDbContext<DatabaseContext>(options => {
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//builder.Services.AddAutoMapper(typeof(MapperInitilizer));
+builder.Services.AddAutoMapper(typeof(MapperInitilizer));
 
 builder.Services.AddCors(options=> {
     options.AddPolicy(name: AllowAll, policy => {
